e93eb5b76a404a0d2de01f3bcd3be43f002a6baf
DyahNanda123 <nandadyah2@gmail.com>
2024-09-20 12:31:47 +0700

    Praktikum 2.7 JS 4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html>
 <head>
     <title>Data User</title>
@@ -12,17 +12,17 @@
             <th>Nama</th>
             <th>ID Level Pengguna</th>
             {{-- <th>Password</th> --}}
-        </tr>
+        {{-- </tr>
         <tr>
         <td>{{ $data->user_id }}</td>
         <td>{{ $data->username }}</td>
         <td>{{ $data->nama }}</td>
-        <td>{{ $data->level_id }}</td>
+        <td>{{ $data->level_id }}</td> --}}
         {{-- <td>{{ $data->password }}</td> --}}
-    </tr>
+    {{-- </tr>
     </table>
 </body>
-</html>  
+</html>   --}} 
 
 {{-- <!DOCTYPE html>
 <html>
@@ -41,3 +41,41 @@
     </table>
 </body>
 </html> --}}
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data User</title>
+</head>
+<body>
+    <h1>Data User</h1>
+    <a href="/PWL_POS/public/user/tambah">Tambah User</a>
+
+    <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <td>ID</td>
+                <td>Username</td>
+                <td>Nama</td>
+                <td>ID Level Pengguna</td>
+                <td>Kode Level</td> 
+                <td>Nama Level</td>
+                <td>Aksi</td>
+            </tr>
+            @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td>{{ $d->level->level_kode }}</td>
+                <td>{{ $d->level->level_nama }}</td>
+                <td>
+                    <a href="/PWL_POS/public/user/ubah/{{ $d->user_id }}">Ubah</a> | 
+                    <a href="/PWL_POS/public/user/hapus/{{ $d->user_id }}">Hapus</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
